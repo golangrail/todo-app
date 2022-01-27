981ca6456b4501b323fa34d40cd5480b2d3019ce
lov3allmy <rail21mur@gmail.com>
2022-01-27 14:22:03 +0500

    Added auth middlewear

---
+++ intenral/delivery/http/middleware.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+const (
+	authorizationHeader = "Authorization"
+	userCtx             = "userID"
+)
+
+func (h *Handler) userIdentity(ctx *gin.Context) {
+	header := ctx.GetHeader(authorizationHeader)
+	if header == "" {
+		newErrorResponse(ctx, http.StatusUnauthorized, "empty auth header")
+		return
+	}
+
+	headerParts := strings.Split(header, " ")
+	if len(headerParts) != 2 {
+		newErrorResponse(ctx, http.StatusUnauthorized, "invalid auth header")
+		return
+	}
+
+	userID, err := h.services.Authorization.ParseToken(headerParts[1])
+	if err != nil {
+		newErrorResponse(ctx, http.StatusUnauthorized, err.Error())
+	}
+
+	ctx.Set(userCtx, userID)
+}
--- intenral/service/auth.go
+++ intenral/service/auth.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/lov3allmy/todo-app/intenral/domain"
@@ -49,6 +50,26 @@ func (s *AuthService) GenerateToken(username, password string) (string, error) {
 	})
 
 	return token.SignedString([]byte(signInKey))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid string method")
+		}
+
+		return []byte(signInKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims.UserID, nil
 }
 
 func generatePasswordHash(password string) string {
