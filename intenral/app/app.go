91eec31ef5e818fcdb3b082db1bd70345a0089d3
lov3allmy <rail21mur@gmail.com>
2022-01-27 10:38:35 +0500

    Added database connection

--- intenral/app/app.go
+++ intenral/app/app.go
@@ -1,12 +1,15 @@
 package app
 
 import (
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	handler "github.com/lov3allmy/todo-app/intenral/delivery/http"
 	"github.com/lov3allmy/todo-app/intenral/repository"
 	"github.com/lov3allmy/todo-app/intenral/server"
 	"github.com/lov3allmy/todo-app/intenral/service"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"os"
 )
 
 func Run() {
@@ -14,7 +17,23 @@ func Run() {
 		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
