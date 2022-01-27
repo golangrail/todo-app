f83dab78dce8d06bf1bcd263611f99b7a31dc807
lov3allmy <rail21mur@gmail.com>
2022-01-27 09:55:19 +0500

    Added config and migration files. Added empty structures of sevices and repository

--- intenral/app/app.go
+++ intenral/app/app.go
@@ -2,14 +2,30 @@ package app
 
 import (
 	handler "github.com/lov3allmy/todo-app/intenral/delivery/http"
+	"github.com/lov3allmy/todo-app/intenral/repository"
 	"github.com/lov3allmy/todo-app/intenral/server"
+	"github.com/lov3allmy/todo-app/intenral/service"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func Run() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(server.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		logrus.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("main")
+	return viper.ReadInConfig()
 }
